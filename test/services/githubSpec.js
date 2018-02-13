82943086be234a80dfd628d3657cebb4a09f04c5
Barry O'Neill <barra.oneill@gmail.com>
2018-02-13 14:54:32 -0500

    fix test run

--- test/services/githubSpec.js
+++ test/services/githubSpec.js
@@ -1,8 +1,7 @@
 const { expect } = require('chai');
-const github = require('../src/services/github');
+const github = require('../../src/services/github');
 
 describe('github service', function () {
-
 
     it('should part revison URL', function () {
 
---
+++ test/set-env.js
@@ -0,0 +1,7 @@
+
+process.env.AWS_REGION='us-east-1';
+process.env.SLACK_CHANNEL='holly-channel';
+process.env.SLACK_WEBHOOK_URI='/my/webhook/uri';
+process.env.GIT_SSM_KEY='/some/ssm/key';
+process.env.SLACK_EMOJI=':thinking_face:';
+process.env.SLACK_USERNAME='holly_mocha';
