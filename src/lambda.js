4c1f8f517371f886b8f6829012799ec4bc434192
Barry O'Neill <barra.oneill@gmail.com>
2018-02-13 22:46:59 -0500

    prevent callbacks from creating dupes

--- src/lambda.js
+++ src/lambda.js
@@ -7,10 +7,10 @@ exports.handler = (event, context, callback) => {
 
     const handler = eventHandlers.find(h => h.handles(event));
     if (handler) {
-        callback(handler.handle(event));
+        callback(null, handler.handle(event));
     }
     else {
-        console.log('No handler for: ' + JSON.stringify(event, null, 2));
+        callback('No handler for: ' + JSON.stringify(event, null, 2));
     }
 
 };
